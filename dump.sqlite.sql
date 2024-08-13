704ed73fa8a52379c71cfaf5c64631fc00098000
Esio <159173984+Esio24@users.noreply.github.com>
2024-08-13 13:46:43 -0300

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE alunos(
+  id int(3)unique,
+nome varchar(30)not null,
+  idade varchar(2)not null,
+  turma varchar(2)not null
+  
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
